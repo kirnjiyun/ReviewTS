f3224f86e9dd2089d2c4172e60723ef63f1ad2b7
kirnjiyun <kimjiyunee@naver.com>
2024-02-18 00:57:16 +0900

    예시

--- 함수/함수 오버로딩.ts
+++ 함수/함수 오버로딩.ts
@@ -1,25 +1,40 @@
-/**
- * 함수 오버로딩
- * 하나의 함수를 매개변수의 개수나 타입에 따라
- * 여러가지 버전으로 만드는 문법
- * 하나의 함수 func
- * 모든 매개변수의 타입 number
- * 어떤 버전이 있는지 먼저 알려줘야해
- */
-//버전들 -> 오버로드 시그니처
-function func(a: number): void;
-function func(a: number, b: number, c: number): void;
+{
+    /**
+     * 함수 오버로딩
+     * 하나의 함수를 매개변수의 개수나 타입에 따라
+     * 여러가지 버전으로 만드는 문법
+     * 하나의 함수 func
+     * 모든 매개변수의 타입 number
+     * 어떤 버전이 있는지 먼저 알려줘야해
+     */
+    function add1(a: number, b: number): number;
+    function add1(a: string, b: string): string;
 
-//실제 구현부 ->구현 시그니처
-function func(a: number, b?: number, c?: number) {
-    if (typeof b === "number" && typeof c === "number") {
-        console.log(a + b + c);
+    function add1(a: number | string, b: number | string): number | string {
+        if (typeof a === "number" && typeof b === "number") {
+            return a + b;
+        } else if (typeof a === "string" && typeof b === "string") {
+            return a.concat(b);
+        } else {
+            throw new Error("Invalid types for addition");
+        }
+    }
+    console.log(add1(1, 2));
+    console.log(add1("14", "00"));
+    console.log(add1("Hello", "World"));
+}
+//오버로드 시그니처
+function greet(name: string): string;
+function greet(name: string, age: number): string;
+
+// 구현 시그니처
+function greet(name: string, age?: number): string {
+    if (age !== undefined) {
+        return `안녕하세요, 제 이름은 ${name}이고, 나이는 ${age}살입니다.`;
     } else {
-        console.log(a * 20);
+        return `안녕하세요, 제 이름은 ${name}입니다.`;
     }
 }
 
-// func();
-func(1);
-// func(1, 2);
-func(1, 2, 3);
+console.log(greet("김지윤")); // "안녕하세요, 제 이름은 김지윤입니다."
+console.log(greet("김지윤", 3)); // "안녕하세요, 제 이름은 김지윤이고, 나이는 3살입니다."
