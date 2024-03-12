7956e0d6e38a3bf1e2e168d385d0690dd120402b
kirnjiyun <kimjiyunee@naver.com>
2024-03-12 21:02:02 +0900

    reducer

--- tsandreact/src/App.tsx
+++ tsandreact/src/App.tsx
@@ -1,23 +1,43 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useReducer, useRef } from "react";
 import Editor from "./components/Editor";
 import { Todo } from "./types";
 import TodoItem from "./components/TodoItem";
 
+type Action =
+    | { type: "CREATE"; data: { id: number; content: string } }
+    | { type: "DELETE"; id: number };
+
+function reducer(state: Todo[], action: Action): Todo[] {
+    switch (action.type) {
+        case "CREATE":
+            return [...state, action.data];
+        case "DELETE":
+            return state.filter((todo) => todo.id !== action.id);
+    }
+}
+
 export default function App() {
-    const idRef = useRef(0);
-    const [todos, setTodos] = useState<Todo[]>([]);
+    const [todos, dispatch] = useReducer(reducer, []);
+    const idRef = useRef(1);
 
     const onClickAdd = (text: string) => {
-        setTodos([...todos, { id: idRef.current++, content: text }]);
+        dispatch({
+            type: "CREATE",
+            data: {
+                id: idRef.current++,
+                content: text,
+            },
+        });
     };
 
     const onClickDelete = (id: number) => {
-        setTodos(todos.filter((todo) => todo.id !== id));
+        dispatch({ type: "DELETE", id: id });
     };
 
     useEffect(() => {
         console.log(todos);
     }, [todos]);
+
     return (
         <div>
             <h1>Todo</h1>
