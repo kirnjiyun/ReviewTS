53855f53f1e88302221741dd82f23c90e973c15c
kirnjiyun <kimjiyunee@naver.com>
2024-03-12 21:42:17 +0900

    context

--- tsandreact/src/App.tsx
+++ tsandreact/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useReducer, useRef } from "react";
+import React, { useContext, useEffect, useReducer, useRef } from "react";
 import Editor from "./components/Editor";
 import { Todo } from "./types";
 import TodoItem from "./components/TodoItem";
@@ -14,6 +14,17 @@ function reducer(state: Todo[], action: Action): Todo[] {
         case "DELETE":
             return state.filter((todo) => todo.id !== action.id);
     }
+}
+export const TodoStateContext = React.createContext<Todo[] | null>(null);
+export const TodoDispatchContext = React.createContext<{
+    onClickAdd: (text: string) => void;
+    onClickDelete: (id: number) => void;
+} | null>(null);
+
+export function useTodoDispatch() {
+    const dispatch = useContext(TodoDispatchContext);
+    if (!dispatch) throw new Error("TodoDispatchContext에 문제가 있다.");
+    return dispatch;
 }
 
 export default function App() {
@@ -41,16 +52,22 @@ export default function App() {
     return (
         <div>
             <h1>Todo</h1>
-            <Editor onClickAdd={onClickAdd} />
-            <div>
-                {todos.map((todo) => (
-                    <TodoItem
-                        key={todo.id}
-                        {...todo}
-                        onClickDelete={onClickDelete}
-                    />
-                ))}
-            </div>
+            <TodoStateContext.Provider value={todos}>
+                <TodoDispatchContext.Provider
+                    value={{ onClickAdd, onClickDelete }}
+                >
+                    <Editor onClickAdd={onClickAdd} />
+                    <div>
+                        {todos.map((todo) => (
+                            <TodoItem
+                                key={todo.id}
+                                {...todo}
+                                // onClickDelete={onClickDelete}
+                            />
+                        ))}
+                    </div>
+                </TodoDispatchContext.Provider>
+            </TodoStateContext.Provider>
         </div>
     );
 }
--- tsandreact/src/components/Editor.tsx
+++ tsandreact/src/components/Editor.tsx
@@ -1,11 +1,15 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import { TodoDispatchContext, useTodoDispatch } from "../App";
 
 interface Props {
     onClickAdd: (text: string) => void;
 }
 
 export default function Editor(props: Props) {
+    // const dispatch = useContext(TodoDispatchContext);
+    const dispatch = useTodoDispatch();
     // const [text, setText] = useState<string>(); //초기값 없어서
+
     const [text, setText] = useState("");
     //미리 정의된 이벤트 객체 타입 가져와야한다.
     const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
